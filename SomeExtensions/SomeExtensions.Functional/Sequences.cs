7807264c58655721f54b573ddb909db153cc1980
JurajDoncevic <Juraj@BELINA>
2020-09-17 20:27:28 +0200

    DoThen added

---
+++ SomeExtensions/SomeExtensions.Functional/Sequences.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SomeExtensions.Functional
+{
+    public static class Sequences
+    {
+        public static TResult DoThen<TResult>(this Action doFirst, Func<TResult> then)
+        {
+            doFirst.Invoke();
+            return then.Invoke();
+        }
+
+        public static TResult DoThen<TInput, TResult>(this Func<TInput> doFirst, Func<TInput, TResult> then) =>
+            then.Invoke(doFirst.Invoke());
+    }
+}
